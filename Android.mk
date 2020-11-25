444f776e7aac3670405a16bd71caf59429d7ffef
Daniel Micay <danielmicay@gmail.com>
2020-11-25 04:11:30 -0500

    Revert "migrate to Android.bp"
    This reverts commit bbdd1310f9ddb746ca7e3c2394d1f1d56f77d86a.

---
+++ Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := Auditor
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRODUCT_MODULE := true
+LOCAL_MULTILIB := both
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := prebuilt/Auditor.apk
+
+include $(BUILD_PREBUILT)
