db379b5495580fd4b5ecd7e0ebacbf91e6dc6454
Daniel Micay <danielmicay@gmail.com>
2022-01-30 19:30:13 -0500

    add optional libraries

--- Android.mk
+++ Android.mk
@@ -7,5 +7,6 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_PRODUCT_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := prebuilt/Auditor.apk
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
 
 include $(BUILD_PREBUILT)
